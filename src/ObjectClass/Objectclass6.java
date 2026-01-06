696ea8ca7e63a37803e702d9972df4ecf4aa7282
dnyanucoder8830 <dnyaneshwaryeware493@gmail.com>
2026-01-06 11:47:26 +0530

    practice Object Class methods

---
+++ src/ObjectClass/Objectclass6.java
@@ -0,0 +1,11 @@
+package ObjectClass;
+class dnyanu{
+    int a=12;
+}
+public class Objectclass6 {
+    public static void main(String[] args) {
+        dnyanu obj=new dnyanu();
+        System.out.println(obj.a);
+        System.out.println(obj.getClass());
+    }
+}
